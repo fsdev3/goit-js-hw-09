3048377ded5bb63cd5f69c826b4dd37b853e483b
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-06-03 18:55:00 +0200

    t2 ok, maybe refactoring

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -7,6 +7,7 @@ const daysEl = document.querySelector('span[data-days]');
 const hoursEl = document.querySelector('span[data-hours]');
 const minutesEl = document.querySelector('span[data-minutes]');
 const secondsEl = document.querySelector('span[data-seconds]');
+let userDate = null;
 
 const options = {
   enableTime: true,
@@ -14,7 +15,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    let userDate = selectedDates[0].getTime();
+    userDate = selectedDates[0].getTime();
     if (userDate < Date.now()) {
       alert('Please choose a date in the future');
       startBtn.disabled = true;
@@ -44,33 +45,29 @@ function convertMs(ms) {
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+let timerId = null;
 
-// function updateDateTime() {
-//   const selectedDate = datetimePicker.selectedDates[0].getTime();
-//   const currentDate = new Date().getTime();
-//   const difference = selectedDate - currentDate;
-//   const { days, hours, minutes, seconds } = convertMs(difference);
-//   daysEl.textContent = addLeadingZero(days);
-//   hoursEl.textContent = addLeadingZero(hours);
-//   minutesEl.textContent = addLeadingZero(minutes);
-//   secondsEl.textContent = addLeadingZero(seconds);
-//   if (difference <= 0) {
-//     clearInterval(timerId);
-//     daysEl.textContent = '00';
-//     hoursEl.textContent = '00';
-//     minutesEl.textContent = '00';
-//     secondsEl.textContent = '00';
-//     return;
-//   }
-// }
-// function addLeadingZero(value) {
-//   return value.toString().padStart(2, '0');
-// }
-// function onBtnClick() {
-//   updateDateTime();
-//   timerId = setInterval(() => updateDateTime(), 1000);
-// }
-// btnEl.addEventListener('click', onBtnClick);
+function updateDateTime() {
+  const difference = userDate - Date.now();
+  const { days, hours, minutes, seconds } = convertMs(difference);
+  daysEl.textContent = addLeadingZero(days);
+  hoursEl.textContent = addLeadingZero(hours);
+  minutesEl.textContent = addLeadingZero(minutes);
+  secondsEl.textContent = addLeadingZero(seconds);
+  if (difference <= 0) {
+    clearInterval(timerId);
+    daysEl.textContent = '00';
+    hoursEl.textContent = '00';
+    minutesEl.textContent = '00';
+    secondsEl.textContent = '00';
+    return;
+  }
+}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+function onClick() {
+  updateDateTime();
+  timerId = setInterval(() => updateDateTime(), 1000);
+}
+startBtn.addEventListener('click', onClick);
